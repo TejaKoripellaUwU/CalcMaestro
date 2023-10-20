c1781a60ab9ac6018c17d038a16dfb2b1aadb67b
bredisrising <ankitmalhotra788@gmail.com>
2023-10-20 14:06:36 -0700

    Revert "added launcher to start ui, several buttons, basic action to grab input"
    This reverts commit 6ae0d21648a96e28db6b8d6ab581725a8b5c83d2.

--- src/UI/MainCalc.java
+++ src/UI/MainCalc.java
@@ -1,68 +1,45 @@
 package UI;
 import javax.swing.JButton;
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-import javax.swing.WindowConstants;
+
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class MainCalc{
-    private JFrame window;
-    private String current_expression = "";
+public class MainCalc extends JFrame{
+    private JTextField txt;
     private GridBagLayout GridBagLayoutGrid;
     private GridBagConstraints gbc;
     private JPanel numPanel = new JPanel();
 
     public MainCalc(){
-        window = new JFrame("Calculator");
-        window.setSize(1000, 800);  
-        window.setPreferredSize(window.getSize()); 
-        GridBagLayout layout = new GridBagLayout();
-        gbc = new GridBagConstraints();    
-        window.setLayout(layout);  
-        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);  
-        decorator();
-        window.setVisible(false);
-        }
+        super();
+        GridBagLayoutGrid = new GridBagLayout();  
+        gbc = new GridBagConstraints();  
+        setLayout(GridBagLayoutGrid);  
+        setTitle("Calculator");  
 
-    public void show(){
-        window.setVisible(!(window.isVisible()));  
-    }
+        GridBagLayout layout = new GridBagLayout();  
+        this.setLayout(layout);  
+        
+        setSize(1000, 800);  
+        setPreferredSize(getSize());  
+        setVisible(true);  
+        setDefaultCloseOperation(EXIT_ON_CLOSE);  
+        createNums();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
 
-    public class add_text implements ActionListener {
-        private String key;
-        public add_text(String s){
-            key = s;
         }
-        public void actionPerformed(ActionEvent e) {
-            current_expression = current_expression + key;
-            System.out.println(current_expression);
-        }
-    }
-    private void decorator(){
-        String[] ops= {"+","-","*","/"};
-        for (int i = 0; i<ops.length;i++){
-            gbc.gridx = i%4;
-            gbc.gridy = (int) i/4;
+    private void createNums(){
+        for (int i = 0; i<10;i++){
+            gbc.gridx = i%3;
+            gbc.gridy = (int) i/3;
             gbc.fill = GridBagConstraints.HORIZONTAL;
-            JButton button = new JButton(ops[i]);
-            button.addActionListener(new add_text(button.getText()));
-            window.add(button);
-
+            
+            this.add(new JButton(Integer.toString(i)),gbc);
         }
-        for (int i = 4; i<13;i++){
-            gbc.gridx = i%4;
-            gbc.gridy = (int) i/4;
-            gbc.fill = GridBagConstraints.HORIZONTAL;
-            JButton button = new JButton(Integer.toString(i-3));
-            button.addActionListener(new add_text(button.getText()));
-            window.add(button,gbc);
-        }
- 
+    
 
     }
 }
